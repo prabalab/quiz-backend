35a841445671dc58140e2c7cc5f0440b54624718
prabalab <40490211+prabalab@users.noreply.github.com>
2026-02-04 16:04:47 +0530

    Update Question.js

--- models/Question.js
+++ models/Question.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
+
+const answerSchema = new mongoose.Schema({
+  text: String,
+  score: Number,
+});
+
+const questionSchema = new mongoose.Schema({
+  questionText: String,
+  answers: [answerSchema],
+});
+
+module.exports = mongoose.model("Question", questionSchema);
 

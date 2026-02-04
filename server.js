3e71b3ba4a42f66f591d24f0fee47c932723186c
prabalab <40490211+prabalab@users.noreply.github.com>
2026-02-04 13:26:40 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+require("./db");
+
+const Question = require("./models/Question");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Quiz API is running 🚀");
+});
+
+app.get("/questions", async (req, res) => {
+  const questions = await Question.find();
+  res.json(questions);
+});
+
+app.post("/questions", async (req, res) => {
+  const question = new Question(req.body);
+  await question.save();
+  res.json({ message: "Question added" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
